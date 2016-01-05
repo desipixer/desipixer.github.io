fabd57c7f6c7cb1f47b6e45c2a0b8b8ce436bef2
Senthil Kumar M <senthilmpro@Senthils-MBP.lan>
2016-01-04 23:25:33 -0800

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit ce72152808c0eb79b64c41af5865d715791c15d9.

--- Scripts/app.js
+++ Scripts/app.js
@@ -14,3 +14,19 @@ app.config(function ($routeProvider) {
 });
 
 google.load("gdata", "2.x");
+
+
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
