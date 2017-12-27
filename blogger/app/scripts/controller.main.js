d45aa430ee1d5a6d26d1fdf817dc3197b95ec92b
senthilmpro <m.senthil.000@gmail.com>
2017-12-27 12:34:09 -0800

    config updated

--- blogger/app/scripts/controller.main.js
+++ blogger/app/scripts/controller.main.js
@@ -21,6 +21,7 @@ app.controller('myCtrl', ['$scope', '$http', 'service.util', '$q', 'service.auth
     };
 
     $scope.configSettings = [
+        "settings8.json",
         "settings7.json",
         "settings6.json",
         "settings5.json",
@@ -190,7 +191,7 @@ app.controller('myCtrl', ['$scope', '$http', 'service.util', '$q', 'service.auth
     /**
      * Gets wpKeys from config files and populates array
      */
-    fetch('config/settings7.json').then(function(response){
+    fetch('config/settings8.json').then(function(response){
         return response.json();
     }).then(function(data){
         if(typeof data == "string"){
