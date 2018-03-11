635cff1b40086e5fa821204132e24f33fafc183d
Senthil Kumar M <m.senthil.000@gmail.com>
2018-03-11 01:14:41 -0800

    new blogs added

--- dumpsterx/dist/js/scripts.js
+++ dumpsterx/dist/js/scripts.js
@@ -679,6 +679,7 @@ app.controller('myCtrl', ['$scope', '$http', 'service.util', '$q', 'service.auth
     };
 
     $scope.configSettings = [
+        "settings5.json",
         "settings4.json",
         "settings3.json",
         "settings.json",
@@ -773,7 +774,7 @@ app.controller('myCtrl', ['$scope', '$http', 'service.util', '$q', 'service.auth
     /**
      * Gets wpKeys from config files and populates array
      */
-    fetch('config/settings3.json').then(function (response) {
+    fetch('config/settings5.json').then(function (response) {
         return response.json();
     }).then(function (data) {
         if (typeof data == "string") {
